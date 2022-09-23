96d7b9d8dcb91f3fac6f65f67f2c7281c349e97d
jginjupa <64933499+jginjupa@users.noreply.github.com>
2022-09-22 20:53:12 -0400

    Add files via upload

---
+++ templatefile.cpp
@@ -0,0 +1,74 @@
+#include "mbed.h"
+
+/*-------------------
+About
+-------------------
+Project Description: Foundation of design and implementation
+Contribitor List:
+Jayaprakash Ginjupalli
+
+--------------------
+Features
+--------------------
+
+--------------------
+Required Materials
+--------------------
+-Nucleo L4R5ZI
+
+--------------------
+Resources and References
+--------------------
+-lecture notes
+
+--------------------
+Getting Started
+--------------------
+This section will describe the general setup
+
+--------------------
+cse321_fall_22_ProvidedCode.cpp:
+--------------------
+ 
+This file has lots of things. There is a os typle tool used to create periodic events with a periepherial. The name of the file and the contents in here will be entirely updated.
+ 
+This is totally not bare metal since there are some cool tools used. Those tools instantiate with a finite reference but gots their unique object created. 
+
+
+----------
+Things Declared
+----------
+Some variables, functions, objects, and APIs are used to make tyhings work. 
+
+There are like 8 of these plus that header .h file.
+
+----------
+API and Built In Elements Used
+----------
+
+----------
+Custom Functions
+----------
+
+hulk:
+	Does savomg ps somentjinmg. 
+	Inputs:
+		None
+	Globally referenced things used:
+	Unicorns and fire, pringtf isglobal too
+*/
+
+
+int main (){
+
+
+//The inputs:
+// Note: for the main file this will be for the peripheral connections.
+//Function-specific inputs should be noted in the comments with the function.
+// The outputs:
+//Note: for the main file this will be for the peripheral connections.
+//Function-specific outputs should be noted in the comments with the function.
+//Any constraints
+// Any sources or references used
+
+}
